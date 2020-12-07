dcffe6bfd4e9fb44c17e06028e020e5031cccfa3
jasonjiangstuy <jasonjiangny@gmail.com>
2020-12-06 22:55:52 -0500

    works with my tester

---
+++ tester.java
@@ -0,0 +1,29 @@
+public class tester {
+    public static void main(String[] args) {
+        IntegerSequence r = new Range(10,20);
+        int count = 0; 
+        // check hasNext, check next
+        while(r.hasNext()){
+            count ++;
+            System.out.print(r.next());
+            if(r.hasNext() ){       
+                System.out.print( ", " );
+            }
+        }
+        // check length
+        System.out.println(r.length() == count);
+        
+        // check reset
+        count = 0;
+        r.reset();
+        while(r.hasNext()){
+            count ++;
+            System.out.print(r.next());
+            if(r.hasNext() ){       
+                System.out.print( ", " );
+            }
+        }
+        System.out.println(r.length() == count);
+        System.out.println();
+    }
+}
